a6c5f2f232adaac25786a4cbc298e8a5d65afaa6
ChechiDev <chechidev@DESKTOP-U70ALR1.localdomain>
2025-10-23 19:46:42 +0200

    Homepage react

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import Ract from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer className="footer py-3 my-3">
+        <hr className="border-bottom" />
+        <p className="text-light text-center">
+          &copy; 2025 - Copyright Reserved by ChechiDev
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
