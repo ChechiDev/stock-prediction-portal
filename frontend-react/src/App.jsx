10a7411f4c65bb6d728fd2748f784ca49330704d
ChechiDev <chechidatadev@gmail.com>
2026-06-21 11:32:14 +0200

    login functionality

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
 import "./assets/css/style.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 import Register from "./components/Register";
 import Login from "./components/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AuthProvider from "./AuthProvider";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <BrowserRouter>
         <Header />
         <Routes>
@@ -19,7 +19,7 @@ function App() {
         </Routes>
         <Footer />
       </BrowserRouter>
-    </>
+    </AuthProvider>
   );
 }
 
